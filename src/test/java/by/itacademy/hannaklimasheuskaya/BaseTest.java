d157417465a426fa71b4951260572baa1468d163
hanna_klimasheuskaya <hanna.klimasheuskaya@gmail.com>
2022-11-29 09:58:11 +0300

    add BaseTest class

---
+++ src/test/java/by/itacademy/hannaklimasheuskaya/BaseTest.java
@@ -0,0 +1,30 @@
+package by.itacademy.hannaklimasheuskaya;
+
+import by.itacademy.hannaklimasheuskaya.page.PizzaMarketPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+    WebDriver driver;
+    @BeforeEach
+    public void setWebDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        PizzaMarketPage pizzaMarketPage = new PizzaMarketPage(driver);
+        driver.manage()
+                .window()
+                .maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        pizzaMarketPage.openPizzaMarketPage();
+        //pizzaMarketPage.clickAtNightButton();
+    }
+    @AfterEach
+    public void closeWebDiver() {
+        driver.quit();
+    }
+}
